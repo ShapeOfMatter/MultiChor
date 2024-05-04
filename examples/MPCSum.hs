e60d4cc20738d07941c28206af457e810f5c63cc
psilospore <syedjafri99@gmail.com>
2024-05-04 11:27:40 -0400

    Move code to Lottery, unbreak master, remove memberToSubset helper

--- examples/MPCSum.hs
+++ examples/MPCSum.hs
@@ -64,10 +64,6 @@ secretShare = do
   secret <- getInput "secret:"
   return (5, secret - 5)
 
--- Random field in [1 to n]
-random :: CLI m (Fp)
-random = undefined
-
 p2pSum :: Choreo Participants (CLI m) ()
 p2pSum = do
   shares1 <- client1 `locally` \_ -> secretShare
@@ -94,31 +90,3 @@ main = do
     cfg = mkHttpConfig [ ("client1",  ("localhost", 4242))
                        , ("client2", ("localhost", 4343))
                        ]
-
--- TODO arbitrary number of clients and participants >= 2 would be nice
--- I'll move this later just like having the above for reference
-lottery :: Choreo Participants (CLI m) ()
-lottery = do
-  -- Create shares
-  client1Shares <- client1 `locally` \_ -> secretShare
-  client2Shares <- client2 `locally` \_ -> secretShare
-
-  -- Client 1 sends shares to all servers {1,2}
-  client1Share1AtServer1 <- (client1, \un -> return $ fst $ un client1 client1Shares) ~~> server1 @@ nobody
-  client1Share2AtServer2 <- (client1, \un -> return $ snd $ un client1 client1Shares) ~~> server2 @@ nobody
-
-  -- Client 2 sends shares to all servers {1,2}
-  client2Share1AtServer1 <- (client1, \un -> return $ fst $ un client2 client2Shares) ~~> server1 @@ nobody
-  client2Share1AtServer2 <- (client1, \un -> return $ snd $ un client2 client2Shares) ~~> server2 @@ nobody
-
-  -- Server each commit to a random value r^j in [1-n]
-  rj1 <- server1 `locally` \_ -> random
-  rj2 <- server2 `locally` \_ -> random
-
-  -- TODO I think there's a step missing because I don't see what we do with the client secrets
-
-  -- The servers each reveal their randomness to all the other servers. R = sum_j(r^j) mod n
-  rj1AtServer2 <- (server1, \un -> return $ un server1 rj1) ~~> server2 @@ nobody
-  rj2AtServer1 <- (server2, \un -> return $ un server2 rj2) ~~> server1 @@ nobody
-
-  pure undefined
