f0a5eb973065235b8f3e4a895c9ffde96aa96767
psilospore <syedjafri99@gmail.com>
2024-05-12 06:55:43 -0400

    remove old lottery

--- examples/Lottery.hs
+++ examples/Lottery.hs
@@ -76,45 +76,5 @@ lottery2 clientsSubsetProof serversSubsetProof = do
   where
     serverNames = toLocs serversSubsetProof
 
--- TODO arbitrary number of clients and participants >= 2 would be nice
--- I'll move this later just like having the above for reference
--- TODO extend to multiple servers later
--- The census is the list of parties present in the cheorography
--- We want to think about how pe9ople use this API
--- Instead of listing the census explicityl
--- Make it polymorphic
--- And instead make this a program where we take proofs that members are part of that polymorphic census
--- e.g. clients are a subset of some set of clients server
-lottery :: forall clients m. (KnownSymbols clients) => Choreo ("server1" ': "server2" ': clients) (CLI m) ()
-lottery = do
-  -- A proof that a subset clients is a subset of clients with servers?
-  let clients = refl
-  s <- clients `parallel` \_ _ -> secretShare
-
-  -- Create shares
-  -- client1Shares <- client1 `locally` (\_ -> secretShare)
-  -- client2Shares <- client2 `locally` (\_ -> secretShare)
-
-  -- Client 1 sends shares to all servers {1,2}
-  -- client1Share1AtServer1 <- (s, \un -> return $ fst $ un client1 client1Shares) ~~> server1 @@ nobody
-  -- client1Share2AtServer2 <- (client1, \un -> return $ snd $ un client1 client1Shares) ~~> server2 @@ nobody
-
-  -- Client 2 sends shares to all servers {1,2}
-  -- client2Share1AtServer1 <- (client2, \un -> return $ fst $ un client2 client2Shares) ~~> server1 @@ nobody
-  -- client2Share1AtServer2 <- (client2, \un -> return $ snd $ un client2 client2Shares) ~~> server2 @@ nobody
-
-  -- Server each commit to a random value r^j in [1-n]
-  -- rj1 <- server1 `locally` \_ -> random
-  -- rj2 <- server2 `locally` \_ -> random
-
-  -- -- TODO I think there's a step missing because I don't see what we do with the client secrets
-
-  -- -- The servers each reveal their randomness to all the other servers. R = sum_j(r^j) mod n
-  -- rj1AtServer2 <- (server1, \un -> return $ un server1 rj1) ~~> server2 @@ nobody
-  -- rj2AtServer1 <- (server2, \un -> return $ un server2 rj2) ~~> server1 @@ nobody
-
-
-  pure undefined
-
 main :: IO ()
 main = undefined
