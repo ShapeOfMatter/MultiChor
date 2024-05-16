df805a4205492022fdf80c371cb953373be3bfeb
Joe Near <jnear@uvm.edu>
2024-05-16 15:18:38 -0400

    start adding actual crypto

--- examples/ObliviousTransfer.hs
+++ examples/ObliviousTransfer.hs
@@ -12,8 +12,8 @@ import Control.Monad.Cont (MonadIO)
 import System.Environment
 --import Logic.Propositional (introAnd)
 import CLI
-import System.Random
-import GHC.TypeLits (KnownSymbol)
+--import GHC.TypeLits (KnownSymbol)
+import Logic.Propositional (introAnd)
 
 import qualified Sel.PublicKey.Cipher as Cipher
 
@@ -36,18 +36,41 @@ $(mkLoc "client2")
 --   client2 `locally_` \un -> putOutput "Total:" $ un client2 total2
 
 
---ot :: (KnownSymbol p1, KnownSymbol p2, MonadIO m) => Choreo '[p1 p2] (CLI m) ()
-ot :: (KnownSymbol p1, KnownSymbol p2, KnownSymbols ps, MonadIO m) => Member p1 ps -> Member p2 ps -> Choreo ps (CLI m) ()
-ot p1 p2 = do
-  v1 <- (p1, \un -> return 5) ~~> p2 @@ nobody
-  return ()
+--ot :: (KnownSymbol p1, KnownSymbol p2, MonadIO m) => Choreo '[p1, p2] (CLI m) ()
+-- ot :: (KnownSymbol p1, KnownSymbol p2, KnownSymbols ps, MonadIO m) => Member p1 ps -> Member p2 ps -> Choreo ps (CLI m) ()
+ot2Insecure :: (MonadIO m) =>
+  Located '["client1"] Bool ->  -- sender
+  Located '["client1"] Bool ->  -- sender
+  Located '["client2"] Bool ->  -- receiver
+  Choreo '["client1", "client2"] (CLI m) (Located '["client2"] Bool)
+ot2Insecure b1 b2 s = do
+  sr <- (client2 `introAnd` client2, s) ~> client1 @@ nobody
+  (client1, \un -> return $ un client1 $ if (un client1 sr) then b1 else b2) ~~> client2 @@ nobody
+
+ot2 :: (MonadIO m) =>
+  Located '["client1"] Bool ->  -- sender
+  Located '["client1"] Bool ->  -- sender
+  Located '["client2"] Bool ->  -- receiver
+  Choreo '["client1", "client2"] (CLI m) (Located '["client2"] Bool)
+ot2 b1 b2 s = do
+  ks1 <- client1 `locally` \_ -> return Cipher.newKeyPair
+  sr <- (client2 `introAnd` client2, s) ~> client1 @@ nobody
+  (client1, \un -> return $ un client1 $ if (un client1 sr) then b1 else b2) ~~> client2 @@ nobody
+
+otTest :: (MonadIO m) => Choreo '["client1", "client2"] (CLI m) ()
+otTest = do
+  b1 <- client1 `locally` \_ -> return False
+  b2 <- client1 `locally` \_ -> return True
+  s <- client2 `locally` \_ -> return False
+  otResult <- ot2 b1 b2 s
+  client2 `locally_` \un -> putOutput "OT output:" $ un client2 otResult
 
 main :: IO ()
 main = do
   [loc] <- getArgs
   delivery <- case loc of
-    "client1"  -> runCLIIO $ runChoreography cfg ot "client1"
-    "client2" -> runCLIIO $ runChoreography cfg ot "client2"
+    "client1"  -> runCLIIO $ runChoreography cfg otTest "client1"
+    "client2" -> runCLIIO $ runChoreography cfg otTest "client2"
     _ -> error "unknown party"
   print delivery
   where
